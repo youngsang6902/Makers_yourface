54caf289bdfcd9f8b729da7615d8c04b8cf78c60
Kim Hyunhee <35857959+Kimhyunhee94@users.noreply.github.com>
2018-05-08 21:20:41 +0900

    Add files via upload

---
+++ visitor.py
@@ -0,0 +1,15 @@
+import os
+
+def check():
+
+	visit = False
+	path = '[directory path]'
+	fileList = os.listdir(path)
+
+	for items in fileList:
+		if items.find('image file name') is not -1:
+			visit = True
+	
+	return visit
+
+
